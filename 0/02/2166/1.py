27ec328906512933b05aa3eb0751a2e98cff784c
seungjoonH <hsj6831@gmail.com>
2025-06-20 20:19:41 +0900

    solved: 2166번 다각형의 면적

--- 0/02/2166/1.py
+++ 0/02/2166/1.py
@@ -2,14 +2,11 @@ from sys import stdin
 input = stdin.readline
 
 def area(a, b, c):
-  ax, ay = a
-  bx, by = b
-  cx, cy = c
-  return .5 * ((ax * by + bx * cy + cx * ay) - (ay * bx + by * cx + cy * ax))
+  (ax, ay), (bx, by), (cx, cy) = a, b, c
+  return ((ax * by + bx * cy + cx * ay) - (ay * bx + by * cx + cy * ax)) / 2
 
 n = int(input())
+coords = [tuple(map(int, input().split())) for _ in range(n)]
 
-x1 = y1 = x2 = y2 = x3 = y3 = 0
-
-for _ in range(n):
-  x, y = map(int, input().split())
+s = sum(area(coords[0], coords[i], coords[i + 1]) for i in range(1, n - 1))
+print(round(abs(s), 1))
