24b9ef86c3e08bb8ee3eb58e57981b891f643aa5
seungjoonH <hsj6831@gmail.com>
2025-08-23 23:57:23 +0900

    chore: 4101번 크냐? 수정

--- 0/04/4101/1.py
+++ 0/04/4101/1.py
@@ -1,3 +1,1 @@
 while'0 0'!=(i:=input()):print(["No","Yes"][eval(i.replace(' ','>'))])
-while(i:=input())!='0 0':a,b=i.split();print(["No","Yes"][a>b])
-while(i:=input())!='0 0':a,b=map(int,i.split());print(["No","Yes"][a>b])
